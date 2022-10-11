15f2f41db4fb0245883fb6858f2af2c5e8f5e0c6
Tarlan Huseynov <tarlan.hosseinov@gmail.com>
2022-10-12 00:34:29 +0400

    going to sleep fuck it

--- scripts/aws_k8s_manifest_bootstrap.sh
+++ scripts/aws_k8s_manifest_bootstrap.sh
@@ -10,3 +10,17 @@ aws_account_id="$(aws sts get-caller-identity --query Account --output text --pr
 aws eks --profile "${profile}" --region "${region}" update-kubeconfig --name eks-cluster    
 kubectl patch deployment coredns -n kube-system --type=json -p='[{"op": "remove", "path": "/spec/template/metadata/annotations", "value": "eks.amazonaws.com/compute-type"}]'
 kubectl rollout restart -n kube-system deployment coredns
+
+#  AWS Load Balancer Controller Service Account
+cat <<EOT | kubectl apply -f -
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  labels:
+    app.kubernetes.io/component: controller
+    app.kubernetes.io/name: aws-load-balancer-controller
+  name: aws-load-balancer-controller
+  namespace: kube-system
+  annotations:
+    eks.amazonaws.com/role-arn: arn:aws:iam::${aws_account_id}:role/AmazonEKSLoadBalancerControllerRole
+EOT
